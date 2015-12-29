a4d92832299078b256e2038b6ca49fc3daa44275
Wouter Verweirder <wouter.verweirder@gmail.com>
2015-12-29 16:33:43 +0100

    cleanup indentation

--- src/ofApp.h
+++ src/ofApp.h
@@ -6,20 +6,20 @@
 
 class ofApp : public ofBaseApp {
 public:
-	void setup();
-	void update();
-	void draw();
+  void setup();
+  void update();
+  void draw();
 
-	int videoWidth;
-	int videoHeight;
-	
-	ofVideoGrabber videoGrabber;
-	ofxPoly2Tri  triangulator;
-	ofPolyline bounds;
-	ofMesh  polypoints;
-	vector<ofPoint> steinerPoints;
+  int videoWidth;
+  int videoHeight;
+  
+  ofVideoGrabber videoGrabber;
+  ofxPoly2Tri  triangulator;
+  ofPolyline bounds;
+  ofMesh  polypoints;
+  vector<ofPoint> steinerPoints;
 
-	ofxCv::FlowFarneback flow;
-	ofMesh mesh;
-	int stepSize, xSteps, ySteps;
+  ofxCv::FlowFarneback flow;
+  ofMesh mesh;
+  int stepSize, xSteps, ySteps;
 };
